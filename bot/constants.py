733d461236159d01cb31d77cdeef26ab27592407
Ruturaj76org <bangaleruturaj111@gmail.com>
2025-04-26 15:06:51 +0530

    Update constants.py

--- bot/constants.py
+++ bot/constants.py
@@ -2,3 +2,9 @@ BUTTONS = {
     "get_song": "Get the song",
     "not_found": "This isn't what I'm looking for"
 }
+
+STATUS_MESSAGES = {
+    "fetching_details": "Fetching the song details... Please wait.",
+    "song_found": "Here is the song you asked for:",
+    "song_not_found": "Sorry, we couldn't find the song you were looking for."
+}
